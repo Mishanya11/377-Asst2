8fa50a4c7a0c98acf78a2ff6840da8e546113199
Michael Rakhamimov <mer0409@gmail.com>
2020-11-06 17:59:23 -0500

    Typeahead
    Created type ahead for script.js

---
+++ public/script.js
@@ -0,0 +1,32 @@
+const restaurants = [];
+
+fetch(endpoint)
+    .then(blob => blob.json)
+    .then(data => restaurants.push(...data))
+
+function findMatches(wordToMatch, restaurants) {
+    return restaurants.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi')
+        return restaurants.name.match(regex) || restaurants.category.match(regex)
+    });
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, restaurants);
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <span class="name">${place.name}</span>
+                <span class="category">${place.category}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+
+const searchInput = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
